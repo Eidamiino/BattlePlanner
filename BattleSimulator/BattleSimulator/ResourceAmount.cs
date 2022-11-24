0e35b68232286d800d14189ad38a1eac8a54b506
Eidamiino <adam.pala199@gmail.com>
2022-11-24 19:46:20 +0100

    merged repos

---
+++ BattleSimulator/BattleSimulator/ResourceAmount.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BattlePlanner
+{
+	public class ResourceAmount
+	{
+		public Resource resource { get;}
+		public int amount { get; }
+		
+		public static List<ResourceAmount> resourceAmountList { get; private set; } =new List<ResourceAmount>();
+
+		public ResourceAmount(Resource resource, int amount)
+		{
+			this.resource = resource;
+			this.amount = amount;
+			resourceAmountList.Add(this);
+
+			if (!Resource.resourceList.Contains(resource))
+				Resource.AddResource(resource);
+		}
+	}
+}
---
+++ BattleSimulator/BattleSimulator/Unit.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Newtonsoft.Json;
+
+namespace BattlePlanner
+{
+	public class Unit
+	{
+		public string Name { get; }
+		public string UnitId { get;}
+		public static List<string> unitIds { get; private set; } = new List<string>();
+		public static List<Unit> unitList { get; private set; } = new List<Unit>();
+		[JsonProperty] public List<ResourceAmount> resourceList { get; private set; } = new List<ResourceAmount>();
+		public Unit(string name)
+		{
+			Name = name;
+			UnitId = GetId(unitIds);
+			AddToUnitList();
+		}
+		public void AddToUnitList()
+		{
+			unitList.Add(this);
+		}
+		public void RemoveUnit()
+		{
+			unitList.Remove(this);
+		}
+		public static Unit FindUnitById(string id)
+		{
+			return unitList.Find(x => x.UnitId.Equals(id));
+		}
+		public void AddToResourceList(Resource resource, int capacity)
+		{
+			ResourceAmount resourceAmount = new ResourceAmount(resource, capacity);
+			resourceList.Add(resourceAmount);
+		}
+		public ResourceAmount FindResourceInList(Resource resource)
+		{
+			return resourceList.Find(x => x.resource.Equals(resource));
+		}
+
+		public void RemoveResource(Resource resource)
+		{
+			ResourceAmount resourceAmount = FindResourceInList(resource);
+			resourceList.Remove(resourceAmount);
+		}
+		public bool CheckIfIdExists(string id)
+		{
+			return unitIds.Contains(id);
+		}
+		public string GetId(List<string> list)
+		{
+			Random rand = new Random();
+			string id;
+			do
+			{
+				int one = rand.Next(1, 10);
+				int two = rand.Next(10, 100);
+				id = one + "-" + two;
+			} while (CheckIfIdExists(id));
+
+			list.Add(id);
+			return id;
+		}
+
+		public override string ToString()
+		{
+			string resources = null;
+			foreach (var item in resourceList)
+			{
+				resources += ($"{item.resource}\n");
+			}
+			return ($"{UnitId} - {Name}\n{resources}");
+		}
+	}
+}
